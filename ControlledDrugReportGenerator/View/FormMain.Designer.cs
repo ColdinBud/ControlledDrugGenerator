c893faefc7b7958147d4c3be7e9529797a4fa80b
Raphael <coldinbud@gmail.com>
2018-05-28 22:44:18 +0800

    first commit, add form

--- ControlledDrugReportGenerator/View/FormMain.Designer.cs
+++ ControlledDrugReportGenerator/View/FormMain.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormMain";
+            this.Name = "FormMain";
+            this.Text = "管制藥報表排程器";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
